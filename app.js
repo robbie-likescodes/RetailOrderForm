891804c36894ea26c9d78019c952ee94c647fb5f
robbie-likescodes <robbie@brewersak.com>
2026-01-22 09:08:43 -0900

    Add bottom back-to-categories button

--- app.js
+++ app.js
@@ -101,6 +101,7 @@ const ui = {
   categoryTitle: $("categoryTitle"),
   categoryMeta: $("categoryMeta"),
   backToCategories: $("backToCategories"),
+  backToCategoriesBottom: $("backToCategoriesBottom"),
 
   pillCategory: $("pillCategory"),
   productName: $("productName"),
@@ -1777,10 +1778,12 @@ function wireEvents() {
   ui.reportsTabBtn?.addEventListener("click", () => setActiveTab("reports"));
 
   ui.reviewBtn?.addEventListener("click", showReview);
-  ui.backToCategories?.addEventListener("click", () => {
+  const returnToCategories = () => {
     state.selectedCategory = "";
     showCatalog();
-  });
+  };
+  ui.backToCategories?.addEventListener("click", returnToCategories);
+  ui.backToCategoriesBottom?.addEventListener("click", returnToCategories);
 
   ui.editBtn?.addEventListener("click", backToWizard);
   ui.submitBtn?.addEventListener("click", submitOrder);
